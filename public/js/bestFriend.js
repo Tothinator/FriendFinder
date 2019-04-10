f7a1fd2a3d01fe8f23e211091c45f6068304ffef
David Toth <dtoth2016@gmail.com>
2019-04-09 22:13:04 -0500

    was doing my bff calcs incorrectly

--- public/js/bestFriend.js
+++ public/js/bestFriend.js
@@ -2,21 +2,19 @@
 module.exports = function (friends, newFriend) {
 
     var bestFriend;
-    var highestScore = 0;
+    var lowestScore = -1;
 
     for (var i = 0; i < friends.length; i++) {
 
         var score = 0;
-        console.log(friends[i]);
+        // console.log(friends[i]);
         for (var j = 0; j < 10; j++) {
-
-            if (friends[i].scores[j] === newFriend.scores[j]){
-                score++;
-            }
+            score = Math.abs(friends[i].scores[j] - newFriend.scores[j])
         }
+    }
 
-        if (score > highestScore) {
-            highestScore = score;
+        if (score < lowestScore || lowestScore === -1) {
+            lowestScore = score;
             bestFriend = i;
         }
     }
